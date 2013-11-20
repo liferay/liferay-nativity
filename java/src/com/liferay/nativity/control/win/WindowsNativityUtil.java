1dfe27cfcb16cf14c2dc303eb291fad15d87569b
gailh <gail.hernandez@liferay.com>
2013-11-20 15:16:21 -0800

    NVTY-46 Enable nativity to load dll without having it in library path

--- java/src/com/liferay/nativity/control/win/WindowsNativityUtil.java
+++ java/src/com/liferay/nativity/control/win/WindowsNativityUtil.java
@@ -16,6 +16,11 @@ package com.liferay.nativity.control.win;
 
 import com.liferay.nativity.util.OSDetector;
 
+import java.io.File;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +30,8 @@ import org.slf4j.LoggerFactory;
 public class WindowsNativityUtil {
 
 	public static boolean load() {
+		_logger.trace("Loading nativity");
+
 		if (!_loaded) {
 			_load();
 		}
@@ -36,6 +43,101 @@ public class WindowsNativityUtil {
 
 	public static native boolean updateExplorer(String filePath);
 
+	private static boolean _createFolder(String destinationFolder) {
+		_logger.trace("Creating folder {} ", destinationFolder);
+
+		File file = new File(destinationFolder);
+
+		if (!file.exists()) {
+			return file.mkdirs();
+		}
+
+		return true;
+	}
+
+	private static String _createPath(String path, String dllName) {
+		_logger.trace("Creating path {} {} ", path, dllName);
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(path);
+
+		if (!path.endsWith("/") && !path.endsWith("\\")) {
+			sb.append("/");
+		}
+
+		sb.append(dllName);
+
+		_logger.trace("Created path {} ", sb.toString());
+
+		return sb.toString();
+	}
+
+	private static void _extractDLL(String destination64, String path64) {
+		_logger.trace("Extracting {} {}", destination64, path64);
+		try {
+			InputStream in = WindowsNativityUtil.class.getResourceAsStream(
+				path64);
+
+			File outFile = new File(destination64);
+
+			FileUtils.copyInputStreamToFile(in, outFile);
+		}
+		catch (Exception e) {
+			_logger.error(e.getMessage(), e);
+		}
+	}
+
+	private static String _extractDLLFromJar() {
+		_logger.trace("Extracting dll from jar ");
+
+		String jarPath = "/";
+
+		String path64 = _createPath(
+			jarPath, _LIFERAY_NATIVITY_WINDOWS_UTIL_x64);
+
+		path64+= _DLL;
+
+		String path86 = _createPath(
+			jarPath, _LIFERAY_NATIVITY_WINDOWS_UTIL_x86);
+
+		path86+= _DLL;
+
+		String dataFolder = System.getenv("APPDATA");
+
+		String destinationFolder = _createPath(dataFolder, "Liferay");
+
+		if (!_createFolder(destinationFolder)) {
+			return null;
+		}
+
+		String destination64 = _createPath(
+			destinationFolder, _LIFERAY_NATIVITY_WINDOWS_UTIL_x64);
+
+		destination64 += _DLL;
+
+		String destination86 = _createPath(
+			destinationFolder, _LIFERAY_NATIVITY_WINDOWS_UTIL_x86);
+
+		destination86 += _DLL;
+
+		if (!_needExtractFromDLL(destination64, destination86)) {
+			_logger.trace("DLL's exist, no need to extract from jar");
+
+			return destinationFolder;
+		}
+
+		_logger.trace("Trying {} {}", destination64, path64);
+
+		_extractDLL(destination64, path64);
+
+		_logger.trace("Trying {} {}", destination86, path86);
+
+		_extractDLL(destination86, path86);
+
+		return destinationFolder;
+	}
+
 	private static void _load() {
 		_loaded = false;
 
@@ -45,16 +147,47 @@ public class WindowsNativityUtil {
 			return;
 		}
 
+		_logger.trace("Starting to load dlls");
+
 		if (!OSDetector.isMinimumWindowsVersion(OSDetector.WIN_VISTA)) {
 			_load = false;
 
+			_logger.error("Unable to load dll, below minimum OS version");
 			return;
 		}
 
 		if (_loadLibrary(false, _LIFERAY_NATIVITY_WINDOWS_UTIL_x64)) {
+			_logger.trace("Successfully loaded x64 libraray from lib.path");
 			return;
 		}
 		else if (_loadLibrary(false, _LIFERAY_NATIVITY_WINDOWS_UTIL_x86)) {
+			_logger.trace("Successfully loaded x86 libraray from lib.path");
+			return;
+		}
+
+		_logger.trace("Unable to load dll from path, loading from jar");
+
+		String path = _extractDLLFromJar();
+
+		if (path == null) {
+			_logger.error("Unable to extrct dll from jar");
+			return;
+		}
+
+		_logger.trace("Extracted dll from jar {} ", path);
+
+		String path64 = _createPath(path, _LIFERAY_NATIVITY_WINDOWS_UTIL_x64);
+		path64 += _DLL;
+
+		String path86 = _createPath(path, _LIFERAY_NATIVITY_WINDOWS_UTIL_x86);
+		path86 += _DLL;
+
+		if (_loadLibrary(true, path64)) {
+			_logger.trace("Loaded {} ", path64);
+			return;
+		}
+		else if (_loadLibrary(true, path86)) {
+			_logger.trace("Loaded {} ", path86);
 			return;
 		}
 
@@ -63,6 +196,7 @@ public class WindowsNativityUtil {
 	}
 
 	private static boolean _loadLibrary(boolean fullPath, String path) {
+		_logger.trace("Trying to load {} {}", fullPath, path);
 		try {
 			if (fullPath) {
 				System.load(path);
@@ -76,8 +210,7 @@ public class WindowsNativityUtil {
 			_logger.trace("Loaded library {}", path);
 		}
 		catch (UnsatisfiedLinkError e) {
-			_logger.error("Failed to load {}", path);
-			_logger.error(e.getMessage(), e);
+			_logger.error("Failed to load {} ()", e.getMessage(), path);
 		}
 		catch (Exception e) {
 			_logger.error("Failed to load {}", path);
@@ -86,6 +219,21 @@ public class WindowsNativityUtil {
 
 		return _loaded;
 	}
+
+	private static boolean _needExtractFromDLL(
+		String destination64, String destination86) {
+
+		File file64 = new File(destination64);
+		File file86 = new File(destination86);
+
+		if (!file64.exists() || !file86.exists()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final String _DLL = ".dll";
 
 	private static final String _LIFERAY_NATIVITY_WINDOWS_UTIL_x64 =
 		"LiferayNativityWindowsUtil_x64";
