483da39f149bf2d218f16f4ca7af74a05a4fc224
Andrew Rondeau <andrew.rondeau@syncplicity.com>
2014-02-19 17:55:42 -0800

    RED-36519 Restored whitespace

--- mac/LiferayNativityFinder/ContentManager.m
+++ mac/LiferayNativityFinder/ContentManager.m
@@ -13,29 +13,29 @@
  */
 
 /**
- * Syncplicity, LLC © 2014
- *
+ * Syncplicity, LLC © 2014 
+ * 
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * 
  * If you would like a copy of source code for this product, EMC will provide a
  * copy of the source code that is required to be made available in accordance
  * with the applicable open source license.  EMC may charge reasonable shipping
  * and handling charges for such distribution.  Please direct requests in writing
  * to EMC Legal, 176 South St., Hopkinton, MA 01748, ATTN: Open Source Program
  * Office.
- *
+ * 
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License along
  * with this library; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * 
  * Changes:
  * - (Andrew Rondeau) Added the ability to group icons by connection, this allows
  * disabling / clearing icons for one program, while leaving another unaffected
@@ -53,13 +53,13 @@ static ContentManager* sharedInstance = nil;
 - init
 {
 	self = [super init];
-	
+
 	if (self)
 	{
 		_fileNamesCacheByConnection = [[NSMapTable alloc] initWithKeyOptions:NSMapTableObjectPointerPersonality valueOptions:NSMapTableStrongMemory capacity:0];
 		_fileIconsEnabled = [[NSHashTable alloc] initWithOptions:NSHashTableObjectPointerPersonality capacity:0];
 	}
-	
+
 	return self;
 }
 
@@ -73,7 +73,7 @@ static ContentManager* sharedInstance = nil;
 	[_fileNamesCacheByConnection release];
 	[_fileIconsEnabled release];
 	sharedInstance = nil;
-	
+
 	[super dealloc];
 }
 
@@ -86,7 +86,7 @@ static ContentManager* sharedInstance = nil;
 			sharedInstance = [[self alloc] init];
 		}
 	}
-	
+
 	return sharedInstance;
 }
 
@@ -100,14 +100,14 @@ static ContentManager* sharedInstance = nil;
 	{
 		[_fileIconsEnabled removeObject:connection];
 	}
-	
+
 	[self repaintAllWindows];
 }
 
 - (NSNumber*)iconByPath:(NSString*)path
 {
 	NSString* normalizedPath = [path decomposedStringWithCanonicalMapping];
-	
+
 	for(id connection in _fileIconsEnabled)
 	{
 		NSDictionary* fileNamesCache = [_fileNamesCacheByConnection objectForKey:connection];
@@ -115,7 +115,7 @@ static ContentManager* sharedInstance = nil;
 		if (nil != fileNamesCache)
 		{
 			NSNumber* result = [fileNamesCache objectForKey:normalizedPath];
-			
+		
 			if (nil != result)
 			{
 				return result;
@@ -129,7 +129,7 @@ static ContentManager* sharedInstance = nil;
 - (void)removeAllIconsFor:(id)connection
 {
 	[_fileNamesCacheByConnection removeObjectForKey:connection];
-	
+
 	[self repaintAllWindows];
 }
 
@@ -142,44 +142,44 @@ static ContentManager* sharedInstance = nil;
 		for (NSString* path in paths)
 		{
 			NSString* normalizedPath = [path decomposedStringWithCanonicalMapping];
-			
+
 			[fileNamesCache removeObjectForKey:normalizedPath];
 		}
 	}
-	
+
 	[self repaintAllWindows];
 }
 
 - (void)repaintAllWindows
 {
 	NSArray* windows = [[NSApplication sharedApplication] windows];
-	
+
 	for (int i = 0; i < [windows count]; ++i)
 	{
 		NSWindow* window = [windows objectAtIndex:i];
-		
+
 		if (![window isVisible])
 		{
 			continue;
 		}
-		
+
 		MenuManager* menuManager = [MenuManager sharedInstance];
 		RequestManager* requestManager = [RequestManager sharedInstance];
-		
+
 		if ([[window className] isEqualToString:@"TBrowserWindow"])
 		{
 			NSObject* browserWindowController = [window browserWindowController];
-			
+
 			BOOL repaintWindow = YES;
-			
+
 			NSString* filterFolder = [requestManager filterFolder];
-			
+
 			if (filterFolder)
 			{
 				repaintWindow = NO;
-				
+
 				struct TFENodeVector* targetPath;
-				
+
 				if ([browserWindowController respondsToSelector:@selector(targetPath)])
 				{
 					// 10.7 & 10.8
@@ -196,24 +196,24 @@ static ContentManager* sharedInstance = nil;
 					
 					return;
 				}
-				
+
 				NSArray* folderPaths = [menuManager pathsForNodes:targetPath];
-				
+
 				for (NSString* folderPath in folderPaths)
 				{
 					if ([folderPath hasPrefix:filterFolder] || [filterFolder hasPrefix:folderPath])
 					{
 						repaintWindow = YES;
-						
+
 						break;
 					}
 				}
 			}
-			
+
 			if (repaintWindow)
 			{
 				NSObject* browserViewController;
-				
+
 				if ([browserWindowController respondsToSelector:@selector(browserViewController)])
 				{
 					// 10.7 & 10.8
@@ -227,12 +227,12 @@ static ContentManager* sharedInstance = nil;
 				else
 				{
 					NSLog(@"LiferayNativityFinder: refreshing icon badges failed");
-					
+
 					return;
 				}
-				
+
 				NSObject* browserView = [browserViewController browserView];
-				
+
 				[browserView setNeedsDisplay:YES];
 			}
 		}
@@ -255,10 +255,10 @@ static ContentManager* sharedInstance = nil;
 		{
 			continue;
 		}
-		
+
 		NSString* normalizedPath = [path decomposedStringWithCanonicalMapping];
 		NSNumber* iconId = [iconDictionary objectForKey:path];
-		
+
 		if ([iconId intValue] == -1)
 		{
 			[fileNamesCache removeObjectForKey:normalizedPath];
@@ -268,7 +268,7 @@ static ContentManager* sharedInstance = nil;
 			[fileNamesCache setObject:iconId forKey:normalizedPath];
 		}
 	}
-	
+
 	if (0 == fileNamesCache.count)
 	{
 		[_fileNamesCacheByConnection removeObjectForKey:connection];
