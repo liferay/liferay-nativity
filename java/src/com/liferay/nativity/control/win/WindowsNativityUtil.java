2f6213bcb394bb23f5aaec42d4af463c50300044
Dennis Ju <dennis.ju@liferay.com>
2016-05-06 11:15:54 -0700

    Add Sync root folders to favorites in Mac, Win 7, and Win 8. Does not work for Win 10's "Quick Access" list

--- java/src/com/liferay/nativity/control/win/WindowsNativityUtil.java
+++ java/src/com/liferay/nativity/control/win/WindowsNativityUtil.java
@@ -37,6 +37,10 @@ public class WindowsNativityUtil {
 		return _loaded;
 	}
 
+	public static native boolean addFavoritesPath(String folder);
+
+	public static native boolean removeFavoritesPath(String folder);
+
 	public static native boolean setSystemFolder(String folder);
 
 	public static native boolean updateExplorer(String filePath);
